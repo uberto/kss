b3917d7344d972371daf537cfe188c88cb94ab4b
Uberto Barbini <uberto.gama@gmail.com>
2018-07-17 18:11:59 +0100

    first block of tests

--- src/test/kotlin/com/gamasoft/kotlin/kss/intro01/FunTest.kt
+++ src/test/kotlin/com/gamasoft/kotlin/kss/intro01/FunTest.kt
@@ -2,16 +2,71 @@ package com.gamasoft.kotlin.kss.intro01
 
 import assertk.assert
 import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
+import assertk.assertions.isNotNull
 import org.junit.jupiter.api.Test
+import java.util.*
 
 class FunTest {
 
     @Test
-    fun defineAFunction(){
+    fun defineAFunctionWithoutAClass(){
 
+        //replace TODO with a value to make the test pass
         fun myfun(): String { return TODO()}
 
         assert(myfun()).isEqualTo("Kotlin is Fun")
     }
+
+    @Test
+    fun valOrVar(){
+
+        var iCanChange = 5
+
+        val iCannotChange = "42".toInt()
+
+        //add something here to make it pass
+
+        assert(iCanChange).isEqualTo(iCannotChange)
+
+    }
+
+    @Test
+    fun createJavaObject(){
+
+        val now: java.util.Date = TODO()
+
+        assert(now).isNotNull()
+
+    }
+
+    @Test
+    fun defaultParameter(){
+
+        fun answer(x: Int){
+            x.toString()
+        }
+
+
+        assert(answer(12)).isEqualTo("12")
+
+//uncomment and make it work
+//        assert(answer()).isEqualTo("42")
+
+    }
+
+
+    @Test
+    fun namedParameters(){
+
+        //replace TODO with a value to make the test pass
+        fun pitagora(hypotenuse: Double, leg: Double): Double = TODO()
+
+        assert(pitagora(5.0, 4.0)).isEqualTo(3.0)
+        assert(pitagora(leg = 4.0, hypotenuse = 5.0)).isEqualTo(3.0)
+
+    }
+
+
 
 }
