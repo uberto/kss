3387fb24fb55ea2c6ba84a70caa2b4a91e3c40eb
uberto <uberto.gama@gmail.com>
2019-04-10 09:34:11 +0100

    Advanced Fun solutions

--- src/test/kotlin/com/ubertob/kotlin/kss/intro08/AdvancedFunTest.kt
+++ src/test/kotlin/com/ubertob/kotlin/kss/intro08/AdvancedFunTest.kt
@@ -9,13 +9,13 @@ class AdvancedFunTest {
 
     tailrec fun sillyMulti(x: Int, times: Int, a: Int = 0): Int {
         return if (times == 0) a
-        else TODO()
+        else sillyMulti(x , times -1, a + x)
     }
 
 
     @Test
     fun tailRecursionSafe(){
-
+        //fix the sillyMulti function to make the test work
         val res = sillyMulti(3, 50000)
 
         assertThat(res).isEqualTo(150000)
@@ -25,7 +25,7 @@ class AdvancedFunTest {
     @Test
     fun classExtension(){
 
-        fun Int.hello(): String = TODO()
+        fun Int.hello(): String = "Hi, I'm $this"
 
         assertThat(5.hello()).isEqualTo("Hi, I'm 5")
         assertThat(42.hello()).isEqualTo("Hi, I'm 42")
@@ -35,7 +35,7 @@ class AdvancedFunTest {
     @Test
     fun nullableClassExtension(){
 
-        fun Int?.hello(): String = TODO()
+        fun Int?.hello(): String = "Hi, I'm " + (this ?: "no one")
 
         assertThat(5.hello()).isEqualTo("Hi, I'm 5")
         assertThat(null.hello()).isEqualTo("Hi, I'm no one")
@@ -45,7 +45,7 @@ class AdvancedFunTest {
     @Test
     fun infixNotation(){
 
-        infix fun <A,B>A.pp(other: B): Pair<A,B> = TODO()
+        infix fun <A,B>A.pp(other: B): Pair<A,B> = Pair(this, other)
 
         assertThat(5 pp 8).isEqualTo(Pair(5, 8))
         assertThat("joe" pp 45).isEqualTo(Pair("joe", 45))
@@ -56,7 +56,7 @@ class AdvancedFunTest {
     @Test
     fun funNames(){
 
-        infix fun <A,B>A.`@`(other: B): Pair<A,B> = TODO()
+        infix fun <A,B>A.`@`(other: B): Pair<A,B> = Pair(this, other)
 
         assertThat(5 `@` 8).isEqualTo(Pair(5, 8))
         assertThat("joe" `@` 45).isEqualTo(Pair("joe", 45))
@@ -65,12 +65,14 @@ class AdvancedFunTest {
 
 
     interface DbContext{
-        fun DbContext.fetchFromDb(key: String):String
+        fun DbContext.fetchFromDb(id: Int):String
     }
 
     class InMemDb: DbContext{
-        override fun DbContext.fetchFromDb(key: String): String {
-            TODO()
+        override fun DbContext.fetchFromDb(id: Int): String = when (id){
+            1 -> "Joe"
+            2 -> "Fred"
+            else -> "No one"
         }
     }
 
@@ -79,9 +81,9 @@ class AdvancedFunTest {
         val dbConn = InMemDb()
 
         dbConn.apply {
-            val userName = fetchFromDb("u1234")
 
-            assertThat(userName).isEqualTo("joe")
+            assertThat(fetchFromDb(1)).isEqualTo("Joe")
+            assertThat(fetchFromDb(2)).isEqualTo("Fred")
         }
 
     }
@@ -97,12 +99,12 @@ class AdvancedFunTest {
     @Test
     fun reifiedGeneric(){
 
-        val s1 = getOrDefault(" ") //find the value to make test pass
+        val s1 = getOrDefault("") //find the value to make test pass
         val s2 = getOrDefault<String>(null)
 
         assertThat(s1).isEqualTo(s2)
 
-        val d1 = getOrDefault(Date(0)) //find the value to make test pass
+        val d1 = getOrDefault(Date()) //find the value to make test pass
         val d2 = getOrDefault<Date>(null)
 
         assertThat(d1.toString()).isEqualTo(d2.toString())
